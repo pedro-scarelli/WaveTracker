e2b9134637d03b785af330ff7a3da9fb1a2dc7ea
pedro-scarelli <pvscarelli@gmail.com>
2025-05-01 22:45:00 -0300

    feat: linted user response dto

--- obj/Debug/net9.0/LoginApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/LoginApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5a3944f17e60379dc554ce12bec310bcd0afc12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48bebffd4cf1e30f6ee1b65e1aa79289e5b622da")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
